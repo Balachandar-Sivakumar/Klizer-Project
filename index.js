c229851f6d743f31afe1e9b8e9e8934562a08cfb
Balachandar-Sivkumar <balachandarsdckap@gmail.com>
2024-09-16 10:31:18 +0530

    Header js

---
+++ index.js
@@ -0,0 +1,11 @@
+function opener(){
+
+    document.getElementById('nav').style.left="30px"
+    
+    }
+    
+    function closer(){
+    
+    document.getElementById('nav').style.left="350px"
+    
+    }
